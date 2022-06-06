60fe12e953af580546038377db3373ac034108eb
Lucky Jain <luckyjain78281@gmail.com>
2022-06-06 14:56:42 +0530

    started numbers and dates

---
+++ Numbers and Dates/script.js
@@ -0,0 +1,6 @@
+console.log(0.1 + 0.3 === 0.3) // false
+console.log(+'64') // converts string to number
+console.log(Number.parseInt('64px')) // 64
+console.log(Number.parseInt('es7')) // NaN
+console.log(Number.parseInt('2.5px')) // 2
+console.log(Number.parseFloat('2.5px')) // 2.5

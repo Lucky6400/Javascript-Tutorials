2dfd4459e5b244fa878712e142400a74bfb0e12c
Lucky Jain <luckyjain78281@gmail.com>
2022-10-10 10:24:03 +0530

    slider fixed

--- Advanced Dom/slider.js
+++ Advanced Dom/slider.js
@@ -2,27 +2,33 @@ const sliderContainer = document.querySelector('.slider_container');
 const slides = document.querySelectorAll('.slide');
 const leftButton = document.querySelector('.slider__btn--left');
 const rightButton = document.querySelector('.slider__btn--right');
+const slider = document.querySelector('.slider');
 
 console.log(slides.length);
+
+console.log(getComputedStyle(sliderContainer).width);
+const translateBy = parseFloat(getComputedStyle(slider).width);
+console.log(translateBy);
+slides.forEach(slide => { slide.style.width = getComputedStyle(slider).width })
 let lastOrFirstSlideDetector = slides.length - 1;
 let translate = 0;
 
 leftButton.addEventListener('click', function(){
     if(translate === 0){
-        translate = -70*lastOrFirstSlideDetector;
+        translate = -translateBy*lastOrFirstSlideDetector;
     } else {
-        translate+=70;
+        translate+=translateBy;
     }
     
-    sliderContainer.style.transform = `translateX(${translate}vw)`
+    sliderContainer.style.transform = `translateX(${translate}px)`
 })
 
 rightButton.addEventListener('click', function(){
-    if(translate === -70*lastOrFirstSlideDetector){
+    if(translate === -translateBy*lastOrFirstSlideDetector){
         translate = 0;
     } else {
-        translate-=70;
+        translate-=translateBy;
     }
     
-    sliderContainer.style.transform = `translateX(${translate}vw)`
+    sliderContainer.style.transform = `translateX(${translate}px)`
 })

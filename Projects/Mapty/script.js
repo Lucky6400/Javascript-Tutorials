c1763fa4a80110bbf7ecc6bc1c7589b784914ff3
Lucky Jain <luckyjain78281@gmail.com>
2022-10-24 01:50:39 +0530

    started mapty project

---
+++ Projects/Mapty/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator?.geolocation?.getCurrentPosition(function (position) {
+    const { latitude, longitude } = position.coords;
+    console.log(latitude, longitude);
+
+    const map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup('You are here')
+        .openPopup();
+}, function () {
+    alert('Geolocation is not available!');
+})

41eedd2efcdf1c83050f2b4fb30559612a2904e5
Lucky Jain <luckyjain78281@gmail.com>
2023-04-13 00:19:57 +0530

    impelmented quick sort

---
+++ Data Structures and Algorithms/Sorting Algorithms/QuickSort.js
@@ -0,0 +1,40 @@
+/** Time Complexity --> O(nlogn) 
+ *  Space Complexity --> 
+ */
+function QuickSort(arr, left = 0, right = arr.length - 1) {
+
+    if (left < right) {
+
+        let pivotIndex = pivot(arr, left, right);
+
+        //left
+        QuickSort(arr, left, pivotIndex - 1);
+
+        //right
+        QuickSort(arr, pivotIndex + 1, right);
+    }
+
+    return arr;
+
+}
+
+console.log(QuickSort([4, 8, 2, 1, 5, 7, 6, 3]));
+
+
+function pivot(array, start = 0, end = array.length + 1) {
+    let pivot = array[start];
+    let swapIndex = start;
+
+    for (let i = start + 1; i < array.length; i++) {
+        if (pivot > array[i]) {
+            swapIndex++;
+            //swapping
+            [array[swapIndex], array[i]] = [array[i], array[swapIndex]];
+        }
+    }
+    //swap start index with pivot index
+    [array[start], array[swapIndex]] = [array[swapIndex], array[start]];
+    return swapIndex;
+}
+
+//console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]))

500a41a953d0e88972832792ac5f518aba7fd24b
Lucky Jain <luckyjain78281@gmail.com>
2023-04-21 23:31:28 +0530

    implemented Tree Traversal

---
+++ Data Structures and Algorithms/Data Structures/TreeTraversal.js
@@ -0,0 +1,136 @@
+/*
+?Two main approaches
+BFS - Breadth first search
+DFS - Depth first search
+*/
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val) {
+        let node = new Node(val);
+        if (!this.root) {
+            this.root = node;
+            return this;
+        }
+        let current = this.root;
+
+        while (true) {
+            if (val === current.val) return undefined;
+            if (val > current.val) {
+                if (current.right === null) {
+                    current.right = node;
+                    return this;
+                }
+                current = current.right;
+            } else {
+                if (current.left === null) {
+                    current.left = node;
+                    return this;
+                }
+                current = current.left;
+            }
+        }
+    }
+
+    find(val) {
+        if (!this.root) return false;
+        let current = this.root;
+        while (current) {
+            if (val === current.val) return current;
+            if (val < current.val) {
+                current = current.left;
+            } else if (val > current.val) {
+                current = current.right;
+            }
+        }
+        return false;
+    }
+
+    invert() {
+        function swap(node) {
+            if (!node) return;
+            let temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            swap(node.left);
+            swap(node.right);
+        }
+        swap(this.root);
+        return this;
+    }
+
+    breadthFirstSearch() {
+        let queue = [this.root];
+        let visited = [];
+        while (queue.length) {
+            let node = queue.shift();
+            visited.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return visited;
+    }
+
+    depthFirstPreOrder() {
+        let visited = [];
+
+        function traverse(node) {
+            visited.push(node.val);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return visited;
+    }
+
+    depthFirstInOrder() {
+        let visited = [];
+
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            visited.push(node.val);
+            if (node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return visited;
+    }
+
+    depthFirstPostOrder() {
+        let visited = [];
+
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            visited.push(node.val);
+        }
+
+        traverse(this.root);
+        return visited;
+    }
+
+}
+
+/*
+?BFS or DFS, which is better? 
+
+BFS is generally better when you want to find the shortest path between two nodes. Since BFS visits all nodes at a given depth before moving on to the next level, it can find the shortest path from the starting node to any other node in the graph or tree.
+
+DFS is generally better when you want to explore all possible paths in a graph or tree. DFS explores each path as far as possible before backtracking, which makes it useful for tasks such as finding all connected components or checking for cycles.
+
+BFS requires more memory than DFS, as it has to keep track of all nodes at a given depth before moving on to the next level. DFS, on the other hand, only needs to keep track of the current path and can therefore use less memory.
+
+The worst-case time complexity for both BFS and DFS is O(V + E), where V is the number of vertices and E is the number of edges. However, the average-case time complexity can vary depending on the structure of the graph or tree. In general, BFS may perform better when the branching factor (the average number of children per node) is low, while DFS may perform better when the branching factor is high.
+*/

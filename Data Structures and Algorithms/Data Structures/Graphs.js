fbd2702b204676c466a984f9e00549d8544cdf47
Lucky Jain <luckyjain78281@gmail.com>
2023-05-13 01:10:00 +0530

    comments refactor

--- Data Structures and Algorithms/Data Structures/Graphs.js
+++ Data Structures and Algorithms/Data Structures/Graphs.js
@@ -6,14 +6,23 @@ mutable) set of vertices or nodes or points, together with a
 set of unordered pairs of these vertices for an undirected 
 graph or a set of ordered pairs for a directed graph.
 
-Graph ek aisa data structure hai jo nodes aur edges se bana hota hai. Iska use different objects ya data points ke beech sambandh darshane ke liye kiya jata hai. Nodes objects ya data points ko darshate hain, jabki edges unke beech ke sambandhon ko darshate hain. Graphs ka use mapping, social networks, aur data analysis jaise various applications ke liye kiya jata hai.
+Graph ek aisa data structure hai jo nodes aur edges se bana hota hai. Iska use different objects ya data 
+points ke beech sambandh darshane ke liye kiya jata hai. Nodes objects ya data points ko darshate hain, jabki 
+edges unke beech ke sambandhon ko darshate hain. Graphs ka use mapping, social networks, aur data analysis 
+jaise various applications ke liye kiya jata hai.
 
 Terminologies:
 Weighted vs Unweighted:
-Weighted aur unweighted graph do alag-alag prakar ke graphs hote hain. Unweighted graph mein, har edge ka same weight hota hai, yaani sabhi edges ka weight 1 hota hai. Lekin weighted graph mein, har edge ka weight alag-alag hota hai aur ye weight positive, negative, ya zero bhi ho sakta hai. Weighted graph mein, edges ke weights ko use karke different algorithms, jaise ki shortest path algorithm, ka use kiya jata hai.
+Weighted aur unweighted graph do alag-alag prakar ke graphs hote hain. Unweighted graph mein, har edge ka 
+same weight hota hai, yaani sabhi edges ka weight 1 hota hai. Lekin weighted graph mein, har edge ka weight 
+alag-alag hota hai aur ye weight positive, negative, ya zero bhi ho sakta hai. Weighted graph mein, edges ke 
+weights ko use karke different algorithms, jaise ki shortest path algorithm, ka use kiya jata hai.
 
 Undirected vs Directed Graph:
-Undirected graph mein, har edge do nodes ko symmetrically connect karta hai, yaani dono nodes ke beech ka sambandh dono taraf se ho sakta hai. Lekin directed graph mein, har edge ke connection directionally hota hai, yaani ek node se dusre node ke taraf hi jata hai. Directed graph mein, nodes aur edges ko arrows se darshaya jata hai jisse edge ka direction darshaya ja sakta hai. 
+Undirected graph mein, har edge do nodes ko symmetrically connect karta hai, yaani dono nodes ke beech ka 
+sambandh dono taraf se ho sakta hai. Lekin directed graph mein, har edge ke connection directionally hota 
+hai, yaani ek node se dusre node ke taraf hi jata hai. Directed graph mein, nodes aur edges ko arrows se 
+darshaya jata hai jisse edge ka direction darshaya ja sakta hai. 
 */
 
 class Graph {
